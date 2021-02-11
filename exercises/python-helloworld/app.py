6c7b2ff60c8ac302819e0fc2fecb75a6a01bcc73
eedygreen <idris.isah2@st.futminna.edu.ng>
2021-02-11 14:42:54 +0100

    added githubwokflows

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,36 @@
 from flask import Flask
+from flast import json
+import logging
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    app.logger.info('Main request successfull')
+    return "Hello Eedy!"
+
+@app.route("/status")
+def status():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+    # logging info
+    app.logger.info('Status request successfull')
+    return response
+
+@app.route("/metrics")
+def metrics():
+    response = app.response_class(
+            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+            status=200,
+            mimetype='application/json'
+    )
+    # loging info
+    app.logger.info('Metrics request successfull')
+    return response
 
 if __name__ == "__main__":
+    # streaming logs to log file app.log
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
     app.run(host='0.0.0.0')
